722b0cfaf71808100f1bb604abf72dea8c260f41
hadeszagros <42893607+mdandre89@users.noreply.github.com>
2021-08-01 15:23:37 +0100

    responsive

--- src/components/Barchart.js
+++ src/components/Barchart.js
@@ -22,79 +22,89 @@ class Barchart extends Component {
       }
     );
 
-    const margin = { top: 30, right: 60, bottom: 30, left: 60 };
-    const width = 800 - margin.left - margin.right;
-    const height = 800 - margin.top - margin.bottom;
+    const redraw = () => {
+      d3.select("#scatterplot svg").remove();
 
-    // setup the canvas
-    const svg = d3
-      .select("#barchart")
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      let headerDiv = document.getElementsByClassName("header");
+      let widthDiv = window.innerWidth;
+      let heightDiv = window.innerHeight - headerDiv[0].clientHeight;
 
-    // create scales and append axis
-    const xScale = d3
-      .scaleBand()
-      .range([0, width])
-      .domain(
-        dataset.map(function (d) {
-          return d.Country;
+      const margin = { top: 30, right: 60, bottom: 30, left: 60 };
+      const width = (widthDiv || 800) - margin.left - margin.right;
+      const height = (heightDiv || 800) - margin.top - margin.bottom;
+
+      // setup the canvas
+      const svg = d3
+        .select("#barchart")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      // create scales and append axis
+      const xScale = d3
+        .scaleBand()
+        .range([0, width])
+        .domain(
+          dataset.map(function (d) {
+            return d.Country;
+          })
+        )
+        .padding(0.3);
+
+      //append x axis
+      svg
+        .append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(xScale))
+        .append("text")
+        .attr("fill", "#000")
+        .attr("x", width + 20)
+        .attr("dy", "-0.71em")
+        .attr("text-anchor", "end")
+        .text("Country");
+
+      const yScale = d3
+        .scaleLinear()
+        .domain([0, d3.max(dataset, (d) => d.Value)])
+        .range([height, 0]);
+
+      //append y axis
+      svg
+        .append("g")
+        .attr("class", "axis axis--y")
+        .attr("transform", `translate(0,0)`)
+        .call(d3.axisLeft(yScale))
+        .append("text")
+        .attr("fill", "#000")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("y coordinate");
+
+      // Bars
+      svg
+        .selectAll("mybar")
+        .data(dataset)
+        .enter()
+        .append("rect")
+        .attr("x", function (d) {
+          return xScale(d.Country);
         })
-      )
-      .padding(0.3);
-
-    //append x axis
-    svg
-      .append("g")
-      .attr("class", "axis axis--x")
-      .attr("transform", `translate(0,${height})`)
-      .call(d3.axisBottom(xScale))
-      .append("text")
-      .attr("fill", "#000")
-      .attr("x", width + 20)
-      .attr("dy", "-0.71em")
-      .attr("text-anchor", "end")
-      .text("Country");
-
-    const yScale = d3
-      .scaleLinear()
-      .domain([0, d3.max(dataset, (d) => d.Value)])
-      .range([height, 0]);
-
-    //append y axis
-    svg
-      .append("g")
-      .attr("class", "axis axis--y")
-      .attr("transform", `translate(0,0)`)
-      .call(d3.axisLeft(yScale))
-      .append("text")
-      .attr("fill", "#000")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", "0.71em")
-      .attr("text-anchor", "end")
-      .text("y coordinate");
-
-    // Bars
-    svg
-      .selectAll("mybar")
-      .data(dataset)
-      .enter()
-      .append("rect")
-      .attr("x", function (d) {
-        return xScale(d.Country);
-      })
-      .attr("y", function (d) {
-        return yScale(d.Value);
-      })
-      .attr("width", xScale.bandwidth())
-      .attr("height", function (d) {
-        return height - yScale(d.Value);
-      })
-      .attr("fill", "#69b3a2");
+        .attr("y", function (d) {
+          return yScale(d.Value);
+        })
+        .attr("width", xScale.bandwidth())
+        .attr("height", function (d) {
+          return height - yScale(d.Value);
+        })
+        .attr("fill", "#69b3a2");
+    };
+    redraw();
+    window.addEventListener("resize", redraw);
   }
   render() {
     return <div id="barchart"></div>;
--- src/components/Scatterplot.js
+++ src/components/Scatterplot.js
@@ -26,77 +26,87 @@ class Scatterplot extends Component {
   }
 
   drawScatterplot(data) {
-    const margin = { top: 30, right: 60, bottom: 30, left: 60 };
-    const width = 800 - margin.left - margin.right;
-    const height = 800 - margin.top - margin.bottom;
+    const redraw = () => {
+      d3.select("#scatterplot svg").remove();
 
-    // setup the canvas
-    const svg = d3
-      .select("#scatterplot")
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      let headerDiv = document.getElementsByClassName("header");
+      let widthDiv = window.innerWidth;
+      let heightDiv = window.innerHeight - headerDiv[0].clientHeight;
 
-    // create scales and append axis
-    const xScale = d3
-      .scaleLinear()
-      .domain([0, d3.max(data, (d) => d[0])])
-      .range([0, width]);
-    //append x axis
-    svg
-      .append("g")
-      .attr("class", "axis axis--x")
-      .attr("transform", `translate(0,${height})`)
-      .call(d3.axisBottom(xScale))
-      .append("text")
-      .attr("fill", "#000")
-      .attr("x", width)
-      .attr("dy", "-0.71em")
-      .attr("text-anchor", "end")
-      .text("x coordinate");
+      const margin = { top: 30, right: 60, bottom: 30, left: 60 };
+      const width = (widthDiv || 800) - margin.left - margin.right;
+      const height = (heightDiv || 800) - margin.top - margin.bottom;
 
-    const yScale = d3
-      .scaleLinear()
-      .domain([0, d3.max(data, (d) => d[1])])
-      .range([height, 0]);
-    //append y axis
-    svg
-      .append("g")
-      .attr("class", "axis axis--y")
-      .attr("transform", `translate(0,0)`)
-      .call(d3.axisLeft(yScale))
-      .append("text")
-      .attr("fill", "#000")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", "0.71em")
-      .attr("text-anchor", "end")
-      .text("y coordinate");
+      // setup the canvas
+      const svg = d3
+        .select("#scatterplot")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    // draw points
-    svg
-      .append("g")
-      .selectAll("circle")
-      .data(data)
-      .enter()
-      .append("circle")
-      .attr("cx", (d) => xScale(d[0]))
-      .attr("cy", (d) => yScale(d[1]))
-      .attr("class", "circles")
-      .attr("r", () => 5);
+      // create scales and append axis
+      const xScale = d3
+        .scaleLinear()
+        .domain([0, d3.max(data, (d) => d[0])])
+        .range([0, width]);
+      //append x axis
+      svg
+        .append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(xScale))
+        .append("text")
+        .attr("fill", "#000")
+        .attr("x", width)
+        .attr("dy", "-0.71em")
+        .attr("text-anchor", "end")
+        .text("x coordinate");
 
-    // draw text labels
-    svg
-      .append("g")
-      .selectAll("text")
-      .data(data)
-      .enter()
-      .append("text")
-      .text((d) => d[0] + "," + d[1])
-      .attr("x", (d) => xScale(d[0]))
-      .attr("y", (d) => yScale(d[1]) - 10);
+      const yScale = d3
+        .scaleLinear()
+        .domain([0, d3.max(data, (d) => d[1])])
+        .range([height, 0]);
+      //append y axis
+      svg
+        .append("g")
+        .attr("class", "axis axis--y")
+        .attr("transform", `translate(0,0)`)
+        .call(d3.axisLeft(yScale))
+        .append("text")
+        .attr("fill", "#000")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("y coordinate");
+
+      // draw points
+      svg
+        .append("g")
+        .selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", (d) => xScale(d[0]))
+        .attr("cy", (d) => yScale(d[1]))
+        .attr("class", "circles")
+        .attr("r", () => 5);
+
+      // draw text labels
+      svg
+        .append("g")
+        .selectAll("text")
+        .data(data)
+        .enter()
+        .append("text")
+        .text((d) => d[0] + "," + d[1])
+        .attr("x", (d) => xScale(d[0]))
+        .attr("y", (d) => yScale(d[1]) - 10);
+    };
+    redraw();
+    window.addEventListener("resize", redraw);
   }
   render() {
     return <div id="scatterplot"></div>;
